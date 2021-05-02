507525810e5e9ba5c20d58d3ee9b9b556427b1f6
Bartek Bimek <bartek.bimek@gmail.com>
2021-05-02 23:04:36 +0200

    working keyboard

--- src/app/keyboard/keyboard.component.ts
+++ src/app/keyboard/keyboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-keyboard',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeyboardComponent implements OnInit {
 
+  @HostListener('window:keydown', ['$event'])
+  onClickDown(event){
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    console.log(document.querySelectorAll(`[key="${event.keyCode}"]`)[0]);
+    let x = document.querySelectorAll(`[key="${event.keyCode}"]`);
+    for(let i = 0; i<x.length; i++){
+      x[i].classList.remove("checked");
+      x[i].classList.add("pressed");
+    }  
+  }
+  @HostListener('window:keyup', ['$event'])
+  onClickUp(event){
+    console.log(document.querySelectorAll(`[key="${event.keyCode}"]`)[0]);
+    let x = document.querySelectorAll(`[key="${event.keyCode}"]`);
+    for(let i = 0; i<x.length; i++){
+      x[i].classList.remove("pressed");
+      x[i].classList.add("checked");
+    }  
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
